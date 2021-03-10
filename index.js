eb9095d4cdfce944a17d1c23d9f5b9064c364a3a
travis_gandy <travis_gandy@yahoo.com>
2021-03-10 17:05:14 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(cards, event) {
+  for (let i = 0; i < cards.length; i++) {
+    console.log(`Thank you, ${cards[i]}, for the wonderful ${event} wishes`);
+  }
+
+  return cards;
+}
+
+function countDown(i) {
+    while ( i > 0) {
+      console.log(i--);
+    }
+  
+    return i;
+  }
